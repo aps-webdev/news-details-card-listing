e3f6f3291c15c98ee331b93fe8d5714b6a2798ce
aps-webdev <amanprataps31@gmail.com>
2023-12-30 12:12:41 +0530

    adding news card component and calling api to add news details

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import NewsDetailCard from "./components/card";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetchAllNewsDeatils();
+  }, []);
+
+  const fetchAllNewsDeatils = async () => {
+    const data = await fetch(
+      "https://newsapi.org/v2/everything?q=bitcoin&from=2023-12-20&sortBy=popularity&apiKey=00f52fd303a742fcb00888b0984c15d1"
+    );
+
+    const result = await data.json();
+    setArticles(result.articles);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {articles.map((article) => {
+        return <NewsDetailCard key={article.publishedAt} article={article} />;
+      })}
     </div>
   );
 }
